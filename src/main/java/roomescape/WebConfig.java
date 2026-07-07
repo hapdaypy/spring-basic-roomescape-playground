815c5110bf5dc01dae9fd208f81587bcaec29a10
hapdaypy <alsdnrskfk85@naver.com>
2026-07-07 21:01:23 +0900

    feat: 일반 사용자 기능과 관리자 기능을 인텁셉터를 도입하여 분리

--- src/main/java/roomescape/WebConfig.java
+++ src/main/java/roomescape/WebConfig.java
@@ -2,7 +2,9 @@ package roomescape;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.AdminInterceptor;
 import roomescape.auth.LoginMemberArgumentResolver;
 
 import java.util.List;
@@ -10,14 +12,21 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final AdminInterceptor adminInterceptor;
 
-    public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver, AdminInterceptor adminInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.adminInterceptor = adminInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
         // 로그인에 이 resolver를 사용해라
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor);
     }
 }
